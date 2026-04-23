4e29ef86e885c14bc242d425df730329242f8365
Tomas Matusek <matusek@foundation.sk>
2026-04-23 15:20:36 +0200

    Added spring init project

---
+++ apps/backend/main/src/main/java/online/tmxlab/commandcenter/service/main/Application.java
@@ -0,0 +1,15 @@
+package online.tmxlab.commandcenter.service.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+
+}
---
+++ apps/backend/main/src/test/java/online/tmxlab/commandcenter/service/main/ApplicationTests.java
@@ -0,0 +1,13 @@
+package online.tmxlab.commandcenter.service.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
